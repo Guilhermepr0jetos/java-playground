cb1f8100c5cdc67b9e64ae360da2f36f27666e05
Jose Guilherme <joseguilhermemmatos@gmail.com>
2024-08-17 18:39:22 -0300

    atualização no conversor de temperatura

--- playground-base/src/com/mycode/ConversorTemperatura.java
+++ playground-base/src/com/mycode/ConversorTemperatura.java
@@ -1,10 +1,18 @@
 package com.mycode;
 public class ConversorTemperatura {
     public static void main(String[] args) {
-        System.out.println("\ntemperatura em kelvin: "+celsiusparaKelvin(40));
+        int celsius = 40;
+
+        int kelvin = celsiusparaKelvin(celsius);
+
+        System.out.println("\ntemperatura em kelvin: "+kelvin+" "+"temperatura em celsius: "+kelvinparaCelsius(kelvin));
     }
 
     public static int celsiusparaKelvin (int celsius) {
         return celsius+273;
+    }
+
+    public static int kelvinparaCelsius (int kelvin) {
+        return kelvin - 273;
     }
 }
